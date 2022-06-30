1c336f14898c1aaccd09ef9092d943c223ab95f3
Mr Fong <75341864+fong-a@users.noreply.github.com>
2022-07-01 08:14:01 +1000

    Setting up classes
    Setting up classes with attributes, methods and instantiating objects.

---
+++ sdd_OOP_python_introduction_skyrim_2.py
@@ -0,0 +1,31 @@
+import random
+
+class Weapon:
+    def __init__(self, name, damage):
+        self.name = name
+        self.damage = damage
+
+class Player:
+    def __init__(self, name, hp):
+        self.weapon = None
+        self.name = name
+        self.hp = hp
+    
+    def attack(self, target):
+        print(f"{self.name} attacks {target.name} using the {self.weapon.name} for {self.weapon.damage}.")
+        
+class Enemy:
+    def __init__(self, name, hp):
+        self.weapon = None
+        self.name = name
+        self.hp = hp
+    
+    def attack(self, target):
+        print(f"{self.name} attacks {target.name} using the {self.weapon.name} for {self.weapon.damage}.")
+
+player = Player("Mr Fong", 100)
+enemy = Enemy("Student", 100)
+sword = Weapon("Silver Sword", 10)
+player.weapon = sword
+player.attack(enemy)
+    
