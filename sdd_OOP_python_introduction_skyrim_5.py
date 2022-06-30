a4bc72ec3dead58c015b30b4703852abcea4848d
Mr Fong <75341864+fong-a@users.noreply.github.com>
2022-07-01 09:04:43 +1000

    Polymorphism
    Demonstration of polymorphism using the 'sharpen' method in three subclasses.

---
+++ sdd_OOP_python_introduction_skyrim_5.py
@@ -0,0 +1,65 @@
+import random
+
+class Weapon:
+    def __init__(self, name, damage):
+        self.name = name
+        self.damage = damage
+
+class Dagger(Weapon):
+    def __init__(self, name, damage):
+        super().__init__(name, damage)
+    def sharpen(self):
+        self.damage += 1
+        print(f"{self.name} is sharpened. The updated attack damage is {self.damage}")
+
+class Sword(Weapon):
+    def __init__(self, name, damage):
+        super().__init__(name, damage)
+    def sharpen(self):
+        self.damage += 4
+        print(f"{self.name} is sharpened. The updated attack damage is {self.damage}")
+
+class Mace(Weapon):
+    def __init__(self, name, damage):
+        super().__init__(name, damage)
+    def imbue(self):
+        self.damage += 10
+        print(f"{self.name} is imbued with holy magic. The updated attack damage is {self.damage}")
+    def sharpen(self):
+        print(f"{self.name} cannot be sharpened, it is a blunt object, duh!")
+
+class Player:
+    def __init__(self, name, hp):
+        self.weapon = None
+        self.name = name
+        self.hp = hp
+    
+    def attack(self, target):
+        print(f"{self.name} attacks {target.name} using the {self.weapon.name} for {self.weapon.damage}.")
+        if self.weapon.damage >= target.hp:
+            target.hp -= self.weapon.damage
+            print(f"{self.name} kills {target.name}!")
+        else:
+            target.hp -= self.weapon.damage
+            print(f"{target.name} has {target.hp} hp remaining!")
+        
+class Enemy:
+    def __init__(self, name, hp):
+        self.weapon = None
+        self.name = name
+        self.hp = hp
+    
+    def attack(self, target):
+        print(f"{self.name} attacks {target.name} using the {self.weapon.name} for {self.weapon.damage}.")
+        target.hp -= self.weapon.damage
+        print(f"{target.name} has {target.hp} remaining!")
+
+player = Player("Mr Fong", 100)
+player_two = Player("Mrs Fong", 100)
+sword = Dagger("Big Sword", 1)
+mace = Mace("Mace of Holy Power", 10)
+
+player.weapon = sword
+player.weapon.sharpen()
+player_two.weapon = mace
+player_two.weapon.sharpen()
