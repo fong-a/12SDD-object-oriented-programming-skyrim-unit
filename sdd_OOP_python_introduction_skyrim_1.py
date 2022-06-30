67980f54bf7be73eec02991e176e452cf7e4c0fb
Mr Fong <75341864+fong-a@users.noreply.github.com>
2022-07-01 08:16:41 +1000

    Boiler plate for students
    Three class definitions to start off with.

---
+++ sdd_OOP_python_introduction_skyrim_1.py
@@ -0,0 +1,20 @@
+class Weapon:
+    def __init__(self):
+        pass
+        # attributes
+        
+        # methods
+
+class Player:
+    def __init__(self):
+        pass
+        # attributes
+        
+        # methods
+
+class Enemy:
+    def __init__(self):
+        pass
+        # attributes
+        
+        # methods
